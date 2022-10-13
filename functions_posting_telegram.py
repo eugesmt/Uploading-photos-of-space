edf39f3af02357bcefcf20cedee54ca11c60ee39
Ujin32 <102483885+Ujin32@users.noreply.github.com>
2022-10-13 15:49:39 +0300

    Устранены замечания после ревью
    1. Доработаны пути для совместимости с *nix и Windows

--- functions_posting_telegram.py
+++ functions_posting_telegram.py
@@ -1,9 +1,12 @@
+from pathlib import Path
+
 import telegram
 
 
 def send_photo_to_channel(image_path, image_name, chat_id, telegram_token):
     bot = telegram.Bot(telegram_token)
-    bot.send_photo(
-        chat_id=chat_id,
-        photo=open(f'{image_path}/{image_name}', 'rb')
-    )
+    with open(Path() / f'{image_path}' / f'{image_name}', 'rb') as photo:
+        bot.send_photo(
+            chat_id=chat_id,
+            photo=photo
+        )
