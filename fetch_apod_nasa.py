af925bd3c2af949d212d4dc4b4f119d9ed747363
Ujin32 <102483885+Ujin32@users.noreply.github.com>
2022-10-13 15:45:15 +0300

    Устранены замечания после ревью
    1. Максимальный размер изображения вынесен в отдельную переменную
    2. Изменены пути на совместимые с *nix и Windows

--- fetch_apod_nasa.py
+++ fetch_apod_nasa.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from pathlib import Path
 
 from dotenv import load_dotenv
 
@@ -8,24 +9,34 @@ from functions_upload_images import check_image_extension, saved_image
 import requests
 
 
-def fetch_apod_nasa(nasa_token, amount_images_apod=None):
-    path_spacex_image = 'images/apod_nasa'
+def fetch_apod_nasa(nasa_token, amount_images_apod):
+    path_spacex_image = Path() / 'images' / 'apod_nasa'
     url_apod_nasa = 'https://api.nasa.gov/planetary/apod'
-    if amount_images_apod is None:
-        amount_images_apod = 1
     payload = {
         'api_key': nasa_token,
         'count': amount_images_apod
     }
     response = requests.get(url=url_apod_nasa, params=payload)
     response.raise_for_status()
-    links_images_apod = response.json()
-    for link_mumber, link in enumerate(links_images_apod):
-        url_image = link['url']
-        extension_image = check_image_extension(url_image)
-        if extension_image:
-            name_apode_image = f'apod_{link_mumber}{extension_image}'
-            saved_image(url_image, path_spacex_image, name_apode_image)
+    apod_nasa_response = response.json()
+    for image_mumber, content in enumerate(apod_nasa_response):
+        media_type = content['media_type']
+        if media_type == 'image':
+            url_image = content['url']
+            extension_image = check_image_extension(url_image)
+            if extension_image:
+                name_apode_image = f'apod_{image_mumber}{extension_image}'
+                params = {
+                    'api_key': nasa_token
+                    }
+                saved_image(
+                    url_image,
+                    path_spacex_image,
+                    name_apode_image,
+                    params=params
+                )
+            else:
+                pass
         else:
             pass
 
@@ -39,14 +50,12 @@ def main():
     args = parser.add_argument(
         '-amount',
         '--amount_images_apod',
+        default=1,
         help='Количество скачиваемых изображений'
     )
 
     args = parser.parse_args()
-    try:
-        fetch_apod_nasa(nasa_token, args.amount_images_apod)
-    except KeyError:
-        pass
+    fetch_apod_nasa(nasa_token, args.amount_images_apod)
 
 
 if __name__ == '__main__':
