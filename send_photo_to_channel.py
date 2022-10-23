341abbb005adf8c297d0be404a59e2c640bd0fc4
Ujin32 <102483885+Ujin32@users.noreply.github.com>
2022-10-23 14:17:31 +0300

    Внесены правки по результатам ревью
    1. collect_files_paths вынесена в отдельный модуль
    2. check_file_size удалена

--- send_photo_to_channel.py
+++ send_photo_to_channel.py
@@ -8,23 +8,6 @@ from dotenv import load_dotenv
 import functions_posting_telegram as posting_tg
 
 
-def collect_files_paths(image_size, images_paths):
-    collected_file_paths = []
-    for image_path in images_paths:
-        if os.path.getsize(image_path) < image_size:
-            collected_file_paths.append(image_path)
-    return collected_file_paths
-
-
-def check_file_size(image_size, image_path):
-    if os.path.getsize(image_path) < image_size:
-        image_path_head, image_path_tail = os.path.split(image_path)
-        file_path = Path() / f'{image_path_head}' / f'{image_path_tail}'
-        return file_path
-    else:
-        return
-
-
 def main():
     image_size = 20971520
     load_dotenv()
@@ -37,19 +20,22 @@ def main():
     args = parser.parse_args()
     image_path = args.image_path
     if image_path is not None:
-        file_path = check_file_size(image_size, image_path)
-        if file_path is not None:
+        if os.path.getsize(image_path) < image_size:
+            image_path_head, image_path_tail = os.path.split(image_path)
+            file_path = Path() / f'{image_path_head}' / f'{image_path_tail}'
             posting_tg.send_photo_to_channel(
                 file_path,
                 chat_id,
                 telegram_token
             )
-        elif file_path is None:
+        else:
             print("Файл слишком большой, выберете другой")
     elif image_path is None:
-        images_paths = posting_tg.collect_files_from_folder()
-        collected_file_paths = collect_files_paths(image_size, images_paths)
-        random_file_to_send = random.choice(collected_file_paths)
+        images_paths = posting_tg.get_files_paths()
+        filtered_files_size = posting_tg.filter_files_size(
+            image_size, images_paths
+        )
+        random_file_to_send = random.choice(filtered_files_size)
         posting_tg.send_photo_to_channel(
             random_file_to_send,
             chat_id,
