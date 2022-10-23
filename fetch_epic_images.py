42c7fe6a72fc27326a0a720374333bd6c1344213
Ujin32 <102483885+Ujin32@users.noreply.github.com>
2022-10-23 14:09:27 +0300

    Внесены правки по результатм ревью
    1. Повышена надежность парсинга даты и времени

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+from datetime import datetime
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -21,12 +22,11 @@ def fetch_epic_images(nasa_token):
     epic_images_link = response.json()
     for epic_image_number, epic_image in enumerate(epic_images_link):
         created_date, image_name = epic_image['date'], epic_image['image']
-        created_yaer, created_month, created_day = created_date.split(' ')[
-            0].split('-')
+        parse_date = datetime.fromisoformat(created_date)
         epic_image_name = f'epic_{epic_image_number}{epic_image_ext}'
         enriched_epic_url = (
             f'https://api.nasa.gov/EPIC/archive/natural/'
-            f'{created_yaer}/{created_month}/{created_day}'
+            f'{parse_date.year}/{parse_date.month}/{parse_date.day}'
             f'/png/{image_name}{epic_image_ext}'
         )
         saved_image(
